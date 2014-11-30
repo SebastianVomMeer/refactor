99310fd0b43727dd9f7518d99463c8c4df57b67a
Sebastian Gaul <sebastian@mgvmedia.com>
2014-11-30 18:38:59 +0100

    Spec: Use local tmp directory

--- spec/group_helpers.rb
+++ spec/group_helpers.rb
@@ -6,7 +6,7 @@ module GroupHelpers
 	end
 
 	def test_project_path
-		'/tmp/test_project'
+		'tmp/test_project'
 	end
 
 	def file_contents(file_path)
@@ -21,7 +21,8 @@ module GroupHelpers
 
 	def create_test_project
 		FileUtils.remove_dir(test_project_path, true)
-    FileUtils.cp_r('spec/test_project', test_project_path)
+		FileUtils.mkdir_p(test_project_path)
+    FileUtils.cp_r('spec/test_project/.', test_project_path)
 	end
 
 	def go_to_test_project_path
